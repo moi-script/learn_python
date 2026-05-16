3c6f31913dafd24f47e357582ffe7941e1405790
lowkeyCodee <rizskplay@gmail.com>
2026-05-16 13:56:33 +0800

    if else in comprehension

--- lists/tasks/t1.py
+++ lists/tasks/t1.py
@@ -195,4 +195,10 @@ def flatten_grid(matrix : list) :
 
 
 
+# [A if condition else B for item in list])
+def transform_numbers(numbers : list) :
+    
+    return [x//2 if x % 2 == 0 else x * 3 for x in numbers]
 
+
+print(transform_numbers([1, 2, 3, 4, 5, 6]))
